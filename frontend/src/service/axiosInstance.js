7938f63c8846259d2470631773c9bf6234a191e0
ThenamXe0n <thenameet0@gmail.com>
2026-05-09 18:07:52 +0530

    changed axios instance baseUrl

--- frontend/src/service/axiosInstance.js
+++ frontend/src/service/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_PUBLIC_API_URL,
+  baseURL: "http://loacalhost:8080",
   withCredentials: true,
 });
 
